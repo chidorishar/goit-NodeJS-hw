654a01f5608346acdef023c4a49346cd474801d2
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-01-13 21:35:01 +0200

    Added API for the local contacts DB

---
+++ contacts.js
@@ -0,0 +1,49 @@
+import { readFileSync } from "node:fs";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const contactsPath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "/db/contacts.json"
+);
+let contacts = [];
+let lastContactIndex;
+
+try {
+  contacts = JSON.parse(readFileSync(contactsPath, "utf-8"));
+} catch (error) {}
+lastContactIndex = contacts.length
+  ? contacts[contacts.length - 1]?.id + 1
+  : (lastContactIndex = 0);
+
+export function listContacts() {
+  console.table(contacts);
+}
+
+export function getContactById(contactId) {
+  const { name, email, phone } =
+    contacts.find(({ id }) => id === contactId) ?? {};
+
+  return !name
+    ? `There is no contact with index of ${contactId}`
+    : `\nName: ${name} \nEmail: ${email} \nPhone: ${phone}`;
+}
+
+export function removeContact(contactId) {
+  contacts = contacts.filter(({ id }) => id !== contactId);
+  writeContactsToDB();
+}
+
+export function addContact(name, email, phone) {
+  contacts.push({ name, email, phone, id: lastContactIndex++ });
+  writeContactsToDB();
+}
+
+async function writeContactsToDB() {
+  try {
+    await writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+  } catch (error) {
+    console.log("Can't write a contacts data to the database to the disk!");
+  }
+}
